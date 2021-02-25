92cf6655b0f03effb6adc79419f73898251cc8e7
urendagj <urendagj@oregonstate.edu>
2021-02-25 09:39:59 -0800

    Factorial functionality added

--- fibonacci.py
+++ fibonacci.py
@@ -2,11 +2,18 @@ import math
 
 def fibonacci(n):
     if n < 0:
-        print("Not in range of fibonacci")
+        raise Exception("Not in range of fibonacci")
     elif n == 0:
         return 0
     elif n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
+def factorial(f):
+    if f < 2:
+        return 1
+    else:
+        return f * factorial(f - 1)
+
 
--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,19 +1,43 @@
 import unittest
 import fibonacci
-
+import pytest
+#unittest
 class TestCase(unittest.TestCase):
     def test_fib1(self):
         self.assertEqual(fibonacci.fibonacci(6), 8)
+
     def test_fib2(self):
         self.assertEqual(fibonacci.fibonacci(4), 3)
+
     def test_fib3(self):
         self.assertEqual(fibonacci.fibonacci(2), 1)
+
     def test_fib4(self):
         self.assertEqual(fibonacci.fibonacci(1), 1)
 
+    def test_fib5(self):
+        with self.assertRaises(TypeError):
+            fibonacci.fibonacci("2")
 
-
+    def test_fib6(self):
+        with self.assertRaises(Exception):
+            fibonacci.fibonacci(-2)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+#pytest
+def test_fibo1():
+    assert fibonacci.fibonacci(0) == 0
+
+def test_fibo2():
+    assert fibonacci.fibonacci(6) == 8
+
+def test_fibo3():
+    with pytest.raises(Exception):
+        fibonacci.fibonacci(-2)
+
+def test_fibo3():
+    with pytest.raises(TypeError):
+        fibonacci.fibonacci("2")
